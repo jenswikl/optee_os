jenswikl/optee_os

commit 2f2f69df5afe70a5e89b70f30281c1aa8ea82716

core: mm: replace MEM_AREA_TA_RAM
Replace MEM_AREA_TA_RAM with MEM_AREA_SEC_RAM_OVERALL.

All read/write secure memory is covered by MEM_AREA_SEC_RAM_OVERALL,
sometimes using an aliased map. But secure read-only or execute core
memory is not covered as that would defeat the purpose of
CFG_CORE_RWDATA_NOEXEC.

Since the partition TA memory isn't accessed via MEM_AREA_TA_RAM any
longer, don't map it using the partition specific map.

This is needed later where unification of OP-TEE core and physical TA
memory is possible.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/core_mmu.c
@@ -66,11 +66,14 @@ static struct tee_mmap_region static_mmap_regions[CFG_MMAP_REGIONS
 #if defined(CFG_CORE_ASLR) || defined(CFG_CORE_PHYS_RELOCATABLE)
 						+ 1
 #endif
-						+ 1] __nex_bss;
+						+ 4] __nex_bss;
 static struct memory_map static_memory_map __nex_data = {
 	.map = static_mmap_regions,
 	.alloc_count = ARRAY_SIZE(static_mmap_regions),
 };
+
+/* Offset of the first TEE RAM mapping from start of secure RAM */
+static size_t tee_ram_initial_offs __nex_bss;
 
 /* Define the platform's memory layout. */
 struct memaccess_area {
@@ -826,8 +829,6 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | tagged;
 	case MEM_AREA_TEE_COHERENT:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRWX | noncache;
-	case MEM_AREA_TA_RAM:
-		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | tagged;
 	case MEM_AREA_NSEC_SHM:
 	case MEM_AREA_NEX_NSEC_SHM:
 		return attr | TEE_MATTR_PRW | cached;
@@ -852,8 +853,9 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 	case MEM_AREA_RAM_NSEC:
 		return attr | TEE_MATTR_PRW | cached;
 	case MEM_AREA_RAM_SEC:
-	case MEM_AREA_SEC_RAM_OVERALL:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | cached;
+	case MEM_AREA_SEC_RAM_OVERALL:
+		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | tagged;
 	case MEM_AREA_ROM_SEC:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PR | cached;
 	case MEM_AREA_RES_VASPACE:
@@ -1074,13 +1076,24 @@ static void collect_mem_ranges(struct memory_map *mem_map)
 {
 	const struct core_mmu_phys_mem *mem = NULL;
 	vaddr_t ram_start = secure_only[0].paddr;
+	size_t n = 0;
 
 #define ADD_PHYS_MEM(_type, _addr, _size) \
 		add_phys_mem(mem_map, #_addr, (_type), (_addr), (_size))
 
 	if (IS_ENABLED(CFG_CORE_RWDATA_NOEXEC)) {
-		ADD_PHYS_MEM(MEM_AREA_TEE_RAM_RO, ram_start,
+		paddr_t next_pa = 0;
+
+		/*
+		 * Read-only and read-execute physical memory areas must
+		 * not be mapped by MEM_AREA_SEC_RAM_OVERALL, but all the
+		 * read/write should.
+		 */
+		ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, ram_start,
 			     VCORE_UNPG_RX_PA - ram_start);
+		assert(VCORE_UNPG_RX_PA >= ram_start);
+		tee_ram_initial_offs = VCORE_UNPG_RX_PA - ram_start;
+		DMSG("tee_ram_initial_offs %#zx", tee_ram_initial_offs);
 		ADD_PHYS_MEM(MEM_AREA_TEE_RAM_RX, VCORE_UNPG_RX_PA,
 			     VCORE_UNPG_RX_SZ);
 		ADD_PHYS_MEM(MEM_AREA_TEE_RAM_RO, VCORE_UNPG_RO_PA,
@@ -1089,15 +1102,30 @@ static void collect_mem_ranges(struct memory_map *mem_map)
 		if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
 			ADD_PHYS_MEM(MEM_AREA_NEX_RAM_RO, VCORE_UNPG_RW_PA,
 				     VCORE_UNPG_RW_SZ);
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, VCORE_UNPG_RW_PA,
+				     VCORE_UNPG_RW_SZ);
+
 			ADD_PHYS_MEM(MEM_AREA_NEX_RAM_RW, VCORE_NEX_RW_PA,
 				     VCORE_NEX_RW_SZ);
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, VCORE_NEX_RW_PA,
+				     VCORE_NEX_RW_SZ);
+
 			ADD_PHYS_MEM(MEM_AREA_NEX_RAM_RW, VCORE_FREE_PA,
 				     VCORE_FREE_SZ);
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, VCORE_FREE_PA,
+				     VCORE_FREE_SZ);
+			next_pa = VCORE_FREE_PA + VCORE_FREE_SZ;
 		} else {
 			ADD_PHYS_MEM(MEM_AREA_TEE_RAM_RW, VCORE_UNPG_RW_PA,
 				     VCORE_UNPG_RW_SZ);
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, VCORE_UNPG_RW_PA,
+				     VCORE_UNPG_RW_SZ);
+
 			ADD_PHYS_MEM(MEM_AREA_TEE_RAM_RW, VCORE_FREE_PA,
 				     VCORE_FREE_SZ);
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, VCORE_FREE_PA,
+				     VCORE_FREE_SZ);
+			next_pa = VCORE_FREE_PA + VCORE_FREE_SZ;
 		}
 
 		if (IS_ENABLED(CFG_WITH_PAGER)) {
@@ -1105,25 +1133,20 @@ static void collect_mem_ranges(struct memory_map *mem_map)
 				     VCORE_INIT_RX_SZ);
 			ADD_PHYS_MEM(MEM_AREA_INIT_RAM_RO, VCORE_INIT_RO_PA,
 				     VCORE_INIT_RO_SZ);
+		} else {
+			ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, next_pa,
+				     secure_only[0].paddr +
+				     secure_only[0].size - next_pa);
 		}
 	} else {
 		ADD_PHYS_MEM(MEM_AREA_TEE_RAM, TEE_RAM_START, TEE_RAM_PH_SIZE);
+		ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, secure_only[n].paddr,
+			     secure_only[0].size);
 	}
 
-	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
-		ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, TRUSTED_DRAM_BASE,
-			     TRUSTED_DRAM_SIZE);
-	} else {
-		/*
-		 * Every guest will have own TA RAM if virtualization
-		 * support is enabled.
-		 */
-		paddr_t ta_base = 0;
-		size_t ta_size = 0;
-
-		core_mmu_get_ta_range(&ta_base, &ta_size);
-		ADD_PHYS_MEM(MEM_AREA_TA_RAM, ta_base, ta_size);
-	}
+	for (n = 1; n < ARRAY_SIZE(secure_only); n++)
+		ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, secure_only[n].paddr,
+			     secure_only[n].size);
 
 	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS) &&
 	    IS_ENABLED(CFG_WITH_PAGER)) {
@@ -1219,7 +1242,7 @@ static bool assign_mem_va_dir(vaddr_t tee_ram_va, struct memory_map *mem_map,
 	 * since it handles virtual memory which covers the part of the ELF
 	 * that cannot fit directly into memory.
 	 */
-	va = tee_ram_va;
+	va = tee_ram_va + tee_ram_initial_offs;
 	for (n = 0; n < mem_map->count; n++) {
 		map = mem_map->map + n;
 		if (map_is_tee_ram(map) ||
@@ -1485,15 +1508,14 @@ static void check_mem_map(struct memory_map *mem_map)
 			if (!pbuf_is_inside(secure_only, m->pa, m->size))
 				panic("TEE_RAM can't fit in secure_only");
 			break;
-		case MEM_AREA_TA_RAM:
+		case MEM_AREA_SEC_RAM_OVERALL:
 			if (!pbuf_is_inside(secure_only, m->pa, m->size))
-				panic("TA_RAM can't fit in secure_only");
+				panic("SEC_RAM_OVERALL can't fit in secure_only");
 			break;
 		case MEM_AREA_NSEC_SHM:
 			if (!pbuf_is_inside(nsec_shared, m->pa, m->size))
 				panic("NS_SHM can't fit in nsec_shared");
 			break;
-		case MEM_AREA_SEC_RAM_OVERALL:
 		case MEM_AREA_TEE_COHERENT:
 		case MEM_AREA_TEE_ASAN:
 		case MEM_AREA_IO_SEC:
@@ -2607,8 +2629,6 @@ early_init(teecore_init_pub_ram);
 
 void core_mmu_init_phys_mem(void)
 {
-	vaddr_t s = 0;
-	vaddr_t e = 0;
 	paddr_t ps = 0;
 	size_t size = 0;
 
@@ -2616,13 +2636,17 @@ void core_mmu_init_phys_mem(void)
 	 * Get virtual addr/size of RAM where TA are loaded/executedNSec
 	 * shared mem allocated from teecore.
 	 */
-	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
-		virt_get_ta_ram(&s, &e);
-	else
-		core_mmu_get_mem_by_type(MEM_AREA_TA_RAM, &s, &e);
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+		vaddr_t s = 0;
+		vaddr_t e = 0;
 
-	ps = virt_to_phys((void *)s);
-	size = e - s;
+		virt_get_ta_ram(&s, &e);
+		ps = virt_to_phys((void *)s);
+		size = e - s;
+
+	} else {
+		core_mmu_get_ta_range(&ps, &size);
+	}
 
 	phys_mem_init(0, 0, ps, size);
 }

core/arch/arm/kernel/boot.c
@@ -381,12 +381,8 @@ static TEE_Result mmap_clear_memtag(struct tee_mmap_region *map,
 				    void *ptr __unused)
 {
 	switch (map->type) {
-	case MEM_AREA_TEE_RAM:
-	case MEM_AREA_TEE_RAM_RW:
 	case MEM_AREA_NEX_RAM_RO:
-	case MEM_AREA_NEX_RAM_RW:
-	case MEM_AREA_TEE_ASAN:
-	case MEM_AREA_TA_RAM:
+	case MEM_AREA_SEC_RAM_OVERALL:
 		DMSG("Clearing tags for VA %#"PRIxVA"..%#"PRIxVA,
 		     map->va, map->va + map->size - 1);
 		memtag_set_tags((void *)map->va, map->size, 0);
@@ -549,8 +545,8 @@ static void init_runtime(unsigned long pageable_part)
 
 	mm = nex_phys_mem_ta_alloc(pageable_size);
 	assert(mm);
-	paged_store = phys_to_virt(tee_mm_get_smem(mm), MEM_AREA_TA_RAM,
-				   pageable_size);
+	paged_store = phys_to_virt(tee_mm_get_smem(mm),
+				   MEM_AREA_SEC_RAM_OVERALL, pageable_size);
 	/*
 	 * Load pageable part in the dedicated allocated area:
 	 * - Move pageable non-init part into pageable area. Note bootloader

core/arch/arm/kernel/secure_partition.c
@@ -1871,7 +1871,7 @@ static const struct ts_ops sp_ops = {
 
 static TEE_Result process_sp_pkg(uint64_t sp_pkg_pa, TEE_UUID *sp_uuid)
 {
-	enum teecore_memtypes mtype = MEM_AREA_TA_RAM;
+	enum teecore_memtypes mtype = MEM_AREA_SEC_RAM_OVERALL;
 	struct sp_pkg_header *sp_pkg_hdr = NULL;
 	struct fip_sp *sp = NULL;
 	uint64_t sp_fdt_end = 0;

core/arch/arm/kernel/virtualization.c
@@ -110,7 +110,7 @@ static size_t get_ta_ram_size(void)
 }
 
 static TEE_Result prepare_memory_map(struct memory_map *mem_map,
-				     paddr_t tee_data, paddr_t ta_ram)
+				     paddr_t tee_data)
 {
 	struct tee_mmap_region *map = NULL;
 	vaddr_t max_va = 0;
@@ -145,18 +145,6 @@ static TEE_Result prepare_memory_map(struct memory_map *mem_map,
 		if (map->va + map->size > max_va)
 			max_va = map->va + map->size;
 	}
-
-	/* Map TA_RAM */
-	mem_map->count++;
-	map = ins_array_elem(mem_map->map, mem_map->count,
-			     sizeof(*mem_map->map), n, NULL);
-	map->region_size = SMALL_PAGE_SIZE;
-	map->va = ROUNDUP(max_va, map->region_size);
-	map->va += (ta_ram - map->va) & CORE_MMU_PGDIR_MASK;
-	map->pa = ta_ram;
-	map->size = get_ta_ram_size();
-	map->type = MEM_AREA_TA_RAM;
-	map->attr = core_mmu_type_to_attr(map->type);
 
 	DMSG("New map (%08lx):",  (vaddr_t)(VCORE_UNPG_RW_PA));
 
@@ -241,8 +229,8 @@ static TEE_Result configure_guest_prtn_mem(struct guest_partition *prtn)
 		goto err;
 	}
 
-	res = prepare_memory_map(&prtn->mem_map, tee_mm_get_smem(prtn->tee_ram),
-				 tee_mm_get_smem(prtn->ta_ram));
+	res = prepare_memory_map(&prtn->mem_map,
+				 tee_mm_get_smem(prtn->tee_ram));
 	if (res)
 		goto err;
 
@@ -576,7 +564,7 @@ void virt_get_ta_ram(vaddr_t *start, vaddr_t *end)
 	struct guest_partition *prtn = get_current_prtn();
 
 	*start = (vaddr_t)phys_to_virt(tee_mm_get_smem(prtn->ta_ram),
-				       MEM_AREA_TA_RAM,
+				       MEM_AREA_SEC_RAM_OVERALL,
 				       tee_mm_get_bytes(prtn->ta_ram));
 	*end = *start + tee_mm_get_bytes(prtn->ta_ram);
 }

core/include/mm/core_mmu.h
@@ -68,7 +68,6 @@
  * MEM_AREA_TEE_COHERENT: teecore coherent RAM (secure, reserved to TEE)
  * MEM_AREA_TEE_ASAN: core address sanitizer RAM (secure, reserved to TEE)
  * MEM_AREA_IDENTITY_MAP_RX: core identity mapped r/o executable memory (secure)
- * MEM_AREA_TA_RAM:   Secure RAM where teecore loads/exec TA instances.
  * MEM_AREA_NSEC_SHM: NonSecure shared RAM between NSec and TEE.
  * MEM_AREA_NEX_NSEC_SHM: nexus non-secure shared RAM between NSec and TEE.
  * MEM_AREA_RAM_NSEC: NonSecure RAM storing data
@@ -98,7 +97,6 @@ enum teecore_memtypes {
 	MEM_AREA_TEE_COHERENT,
 	MEM_AREA_TEE_ASAN,
 	MEM_AREA_IDENTITY_MAP_RX,
-	MEM_AREA_TA_RAM,
 	MEM_AREA_NSEC_SHM,
 	MEM_AREA_NEX_NSEC_SHM,
 	MEM_AREA_RAM_NSEC,
@@ -133,7 +131,6 @@ static inline const char *teecore_memtype_name(enum teecore_memtypes type)
 		[MEM_AREA_TEE_ASAN] = "TEE_ASAN",
 		[MEM_AREA_IDENTITY_MAP_RX] = "IDENTITY_MAP_RX",
 		[MEM_AREA_TEE_COHERENT] = "TEE_COHERENT",
-		[MEM_AREA_TA_RAM] = "TA_RAM",
 		[MEM_AREA_NSEC_SHM] = "NSEC_SHM",
 		[MEM_AREA_NEX_NSEC_SHM] = "NEX_NSEC_SHM",
 		[MEM_AREA_RAM_NSEC] = "RAM_NSEC",

core/kernel/ree_fs_ta.c
@@ -726,7 +726,7 @@ static TEE_Result buf_ta_open(const TEE_UUID *uuid,
 		goto err;
 	}
 	handle->buf = phys_to_virt(tee_mm_get_smem(handle->mm),
-				   MEM_AREA_TA_RAM, handle->ta_size);
+				   MEM_AREA_SEC_RAM_OVERALL, handle->ta_size);
 	if (!handle->buf) {
 		res = TEE_ERROR_OUT_OF_MEMORY;
 		goto err;

core/mm/fobj.c
@@ -270,7 +270,8 @@ static struct fobj *rwp_unpaged_iv_alloc(unsigned int num_pages)
 	mm = nex_phys_mem_ta_alloc(size);
 	if (!mm)
 		goto err_free_state;
-	rwp->store = phys_to_virt(tee_mm_get_smem(mm), MEM_AREA_TA_RAM, size);
+	rwp->store = phys_to_virt(tee_mm_get_smem(mm),
+				  MEM_AREA_SEC_RAM_OVERALL, size);
 	assert(rwp->store);
 
 	fobj_init(&rwp->fobj, &ops_rwp_unpaged_iv, num_pages);
@@ -395,7 +396,7 @@ static TEE_Result rwp_init(void)
 	assert(rwp_state_base);
 
 	rwp_store_base = phys_to_virt(nex_phys_mem_get_ta_base(),
-				      MEM_AREA_TA_RAM, ta_size);
+				      MEM_AREA_SEC_RAM_OVERALL, ta_size);
 	assert(rwp_store_base);
 
 	return TEE_SUCCESS;
@@ -766,7 +767,8 @@ struct fobj *fobj_sec_mem_alloc(unsigned int num_pages)
 	if (!f->mm)
 		goto err;
 
-	va = phys_to_virt(tee_mm_get_smem(f->mm), MEM_AREA_TA_RAM, size);
+	va = phys_to_virt(tee_mm_get_smem(f->mm), MEM_AREA_SEC_RAM_OVERALL,
+			  size);
 	if (!va)
 		goto err;
 

core/mm/pgt_cache.c
@@ -112,7 +112,8 @@ static struct pgt_parent *alloc_pgt_parent(void)
 		free(parent);
 		return NULL;
 	}
-	tbl = phys_to_virt(tee_mm_get_smem(parent->mm), MEM_AREA_TA_RAM,
+	tbl = phys_to_virt(tee_mm_get_smem(parent->mm),
+			   MEM_AREA_SEC_RAM_OVERALL,
 			   PGT_PARENT_SIZE);
 	assert(tbl); /* "can't fail" */
 

commit e41d922bd2c52ce91dd54593847285b4b20f960a

core: fix panic with TEE_SDP_TEST_MEM
The commit 2f2f69df5afe ("core: mm: replace MEM_AREA_TA_RAM") uses
MEM_AREA_SEC_RAM_OVERALL to map practically all secure memory. This
conflicts with TEE_SDP_TEST_MEM:
1. MEM_AREA_SEC_RAM_OVERALL covers TEE_SDP_TEST_MEM and triggers a panic
   in verify_special_mem_areas().
2. TEE_SDP_TEST_MEM_BASE refers to physical memory only since it's only
   mapped in MEM_AREA_SEC_RAM_OVERALL.

So fix these problems.

Fixes: 2f2f69df5afe ("core: mm: replace MEM_AREA_TA_RAM")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/core_mmu.c
@@ -681,6 +681,12 @@ static void verify_special_mem_areas(struct memory_map *mem_map,
 	 */
 	for (mem = start; mem < end; mem++) {
 		for (n = 0; n < mem_map->count; n++) {
+			/*
+			 * Ignore MEM_AREA_SEC_RAM_OVERALL since it can
+			 * cover all secure memory, even if it's special.
+			 */
+			if (mem_map->map[n].type == MEM_AREA_SEC_RAM_OVERALL)
+				continue;
 			if (core_is_buffer_intersect(mem->addr, mem->size,
 						     mem_map->map[n].pa,
 						     mem_map->map[n].size)) {
@@ -2776,7 +2782,7 @@ void core_mmu_init_phys_mem(void)
 		/* Carve out test SDP memory */
 #ifdef TEE_SDP_TEST_MEM_BASE
 		if (TEE_SDP_TEST_MEM_SIZE) {
-			pa = vaddr_to_phys(TEE_SDP_TEST_MEM_BASE);
+			pa = TEE_SDP_TEST_MEM_BASE;
 			carve_out_core_mem(pa, pa + TEE_SDP_TEST_MEM_SIZE);
 		}
 #endif